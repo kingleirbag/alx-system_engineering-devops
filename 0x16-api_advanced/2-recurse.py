0d34928bda8c8508ff4762afa0e671a6d1ba183c
Gabriel Muthenya <gmthex@gmail.com>
2023-10-12 12:25:05 -0700

    COint

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-A recursive function that queries the Reddit API
-and returns a list containing the
-titles of all hot articles for a given subreddit.
+A recursive function that queries the Reddit API and returns a list 
+containing the titles of all hot articles for a given subreddit.
 """
 import requests
 after = None
