4314fc4b8685e826ecde3ba16bea0acf294f8ee7
Gabriel Muthenya David <gmthex@gmail.com>
2023-10-14 15:58:10 +0000

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Fix "phpp" file to "php"
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
