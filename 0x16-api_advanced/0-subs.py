c40edefa6fbd01e784af6d393d836cda1ee37473
Gabriel Muthenya <gmthex@gmail.com>
2024-01-16 10:12:25 -0800

    added subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""A function that queries subscribers on a given Reddit subreddit"""
+"""Function to query number  subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit"""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
